e2312fa31aeea479446d0aefd0b5297f67103fe6
brenofflank <brenoocanha@hotmail.com>
2023-05-17 16:16:48 -0300

    up project structure

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,28 +1,24 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
 import styled from 'styled-components';
-import { Card, CardHeader, CardBody, CardFooter } from '@chakra-ui/react'
+import { Heading } from '@chakra-ui/react'
+import { Text } from '@chakra-ui/react'
+import { Box } from '@chakra-ui/react';
 
-const AboutContainer = styled.div`
-  background-color: white;
-  display: flex;
-  width: 100%;
-  height: 100vh;
+const StyledBox = styled(Box)`
+  margin: 10% 20%;
+  border: 1px solid black;
+  padding: 20px;
+  border-radius: 5px;
 `
-const StyledCard = styled(Card)`
-  margin: 10% 10%;
-  
-`;
-
 
 function About() {
   return (
-    <div>
-        <StyledCard>
-          <CardHeader>Teste</CardHeader>
-          <CardBody>Teste Teste Teste Teste</CardBody>
-        </StyledCard>
-    </div>
+
+    <StyledBox>
+        <Heading mb={4}>About The Statistic Doggo Project</Heading>
+        <Text fontSize='xl'>
+        The Statistic Doggo started as a college project made by two System Analysis and Development students called Breno and Marcos. This project is all about statistic, and it started in May 2023. The objective of this project is to create a statistic calculator and graphic drawer site, used for studies and projects. </Text>
+    </StyledBox>
   );
 }
 
--- src/pages/Statistic.js
+++ src/pages/Statistic.js
@@ -1,15 +1,31 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
-// import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
 
 
 
 function Statistic() {
   return (
-    <div>
-      <h1>Statistic Page</h1>
-      <p>Esta é a página de Estatísticas.</p>
-    </div>
+    <Tabs>
+    <TabList>
+      <Tab>Média</Tab>
+      <Tab>Two</Tab>
+      <Tab>Three</Tab>
+    </TabList>
+
+    <TabPanels>
+      <TabPanel>
+        <p>Média Aqui</p>
+      </TabPanel>
+      <TabPanel>
+        <p>two!</p>
+      </TabPanel>
+      <TabPanel>
+        <p>three!</p>
+      </TabPanel>
+    </TabPanels>
+  </Tabs>
   );
 }
 
